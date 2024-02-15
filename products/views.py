fc50a2e53b35e82afc536aa6d780041288cc612b
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-02-15 11:45:31 +0000

    Add category filtering

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 # Import query library for search form
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 # Create your views here.
 
 
@@ -13,28 +13,41 @@ def all_products(request):
     """
     # Get all products
     products = Product.objects.all()
-    # Set search term to None to prevent errors
+    # Set search term and category to None to prevent errors
     query = None
+    categories = None
 
     # Check if GET action performed
     if request.GET:
-        # Check if there was a search term entered
-        query = request.GET['q']
-        if not query:
-            # Create an error message to be displayed on the page
-            messages.error(request, "You didn't enter any search criteria")
-            return redirect(reverse('products'))
+        # Check if a specific category was selected
+        if 'category' in request.GET:
+            # If more than one category in the comma-separated list, then
+            # split the list
+            categories = request.GET['category'].split(',')
+            # Filter products according to categories
+            products = products.filter(category__name__in=categories)
+            # Get category names according to the filter
+            categories = Category.objects.filter(name__in=categories)
 
-        # As a standard search searches both product name AND description,
-        # use Q function imported above to search OR. The i in the query
-        # means it is a case insensitive search.
-        queries = Q(name__icontains=query) | Q(description__icontains=query)
-        # Filter products based on the query
-        products = products.filter(queries)
+        # Check if there was a search term entered
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                # Create an error message to be displayed on the page
+                messages.error(request, "You didn't enter any search criteria")
+                return redirect(reverse('products'))
+
+            # As a standard search searches both product name AND description,
+            # use Q function imported above to search OR. The i in the query
+            # means it is a case insensitive search.
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            # Filter products based on the query
+            products = products.filter(queries)
 
     context = {
         "products": products,
         'search_term': query,
+        'current_categories': categories,
     }
     return render(request, "products/products.html", context)
 
