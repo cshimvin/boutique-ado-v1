8edd68cd7e8f777dba0920d550b576d25989846b
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-03-04 10:15:36 +0000

    Add size information to bag

--- bag/views.py
+++ bag/views.py
@@ -14,12 +14,31 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    # check if there is a size
+    if size:
+        if item_id in list(bag.keys()):
+            # if an item of that size already exists,
+            # increase quantity by 1
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                # otherwise if item is in the bag but a new size
+                # has been added, add the item with the new size
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            # If a new item is added with a size, add to bag
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        # if item doesn't have a size, add to bag without size
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
