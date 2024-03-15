8bac4f9c884baab5e77520265fdab57140185df9
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-03-15 12:15:46 +0000

    Add country dropdown to checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -25,7 +25,6 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
@@ -37,13 +36,14 @@ class OrderForm(forms.ModelForm):
         self.fields['full_name'].widget.attrs['autofocus'] = True
         # Iterate through form fields
         for field in self.fields:
-            # If it's a required field add a *
-            # Add placeholder text to each field
-            # Remove label from each field
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != "country":
+                # If it's a required field add a *
+                # Add placeholder text to each field
+                # Remove label from each field
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
