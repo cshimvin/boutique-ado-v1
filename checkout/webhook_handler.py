f7cd917f7dfc1986b9dbba7b803c39616b6e3c9d
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-03-18 10:59:51 +0000

    Add product form in product admin

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -10,7 +10,6 @@ from profiles.models import UserProfile
 import json
 import time
 
-
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
 
@@ -18,7 +17,7 @@ class StripeWH_Handler:
         self.request = request
 
     def _send_confirmation_email(self, order):
-        """ Send the user a confirmation email """
+        """Send the user a confirmation email"""
         cust_email = order.email
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
@@ -26,13 +25,13 @@ class StripeWH_Handler:
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-
+        
         send_mail(
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
             [cust_email]
-        )
+        )        
 
     def handle_event(self, event):
         """
