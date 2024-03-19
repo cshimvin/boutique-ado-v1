ecc7df265e96879126dfc6dfc84539c0da8463d9
Christopher Shimmin-Vincent <cshimminvincent@gmail.com>
2024-03-19 19:40:38 +0000

    Add email functionality

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -228,5 +228,10 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # email
-DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
-ACCOUNT_EMAIL_VERIFICATION = 'none'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
